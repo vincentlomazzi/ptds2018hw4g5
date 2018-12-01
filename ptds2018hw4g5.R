42a01e3f98d03b1cd9844eaea439eb3a4958bcd9
vincentlomazzi <vincent.lomazzi@unil.ch>
2018-12-01 21:48:53 +0100

    Created is_inside.cpp in /src folder and ran the asked function

---
+++ ptds2018hw4g5.R
@@ -0,0 +1,3 @@
+#' @useDynLib ptds2018hw4g5
+#' @importFrom Rcpp sourceCpp
+NULL
---
+++ src/is_inside.cpp
@@ -0,0 +1,12 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+LogicalVector is_inside(NumericMatrix points) {
+
+  LogicalVector inside(points.nrow());
+
+  // for loop in which `inside` is defined
+
+  return inside;
+}
