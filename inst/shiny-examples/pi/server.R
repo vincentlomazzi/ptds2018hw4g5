710a55049879a11945de7ba2f4519c9a477d588c
Upsylon <patrik.grandadam@unil.ch>
2018-12-02 18:05:49 +0100

    Addition of the server file in ShinyApp

--- inst/shiny-examples/pi/server.R
+++ inst/shiny-examples/pi/server.R
@@ -1,26 +1,45 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ptds2018hw4g5)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+
+
+  simulate <- eventReactive(input$button, {
+    # simulate pi and measure the time here
+
+    if (input$method == "estimate_pi") {
+      estimated_pi <- estimate_pi(B = input$B, seed = input$seed)
+    } else {
+      estimated_pi <- estimate_pi2(B = input$B, seed = input$seed)
+    }
+    return(estimated_pi)
+
   })
-  
-})
+
+  simulated_time <- eventReactive(input$button, {
+    if (input$method == "estimate_pi") {
+      system.time({ estimate_pi(B = input$B, seed = input$seed) })
+    } else {
+      system.time({ estimate_pi2(B = input$B, seed = input$seed) })
+    }
+  })
+
+  output$plot <- renderPlot({
+    plot(simulate())
+  })
+
+  output$time <- renderText({
+    # extract the time of the execution
+    paste("The time to run find the estimation of pi for the user is equal to",
+          round(simulated_time()[3], 4), "seconds."
+    )
+  })
+
+  output$pi <- renderText({
+    # extract the estimated value
+    paste("The estimation for pi is equal to",
+          round(simulate()[[1]], 6)
+    )
+  })
+}
+)
